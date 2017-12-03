525a52ee2550bff05bf46059296924d30a1baaf3
Allen Garvey <allen.garvey@gmail.com>
2017-12-02 21:02:52 -0500

    Add functionality to either accept input file as argument, or read from stdin

--- src/main.d
+++ src/main.d
@@ -1,16 +1,51 @@
 module dminjs.main;
 
 import std.stdio;
+import std.file;
+import core.sys.posix.unistd;
 import dminjs.minify;
 
-void main(){
+int main(string[] args){
+    File inputFile;
+    
+    //check if input is being piped
+    if(!isatty(stdin.fileno)){
+        inputFile = stdin;
+    }
+    else{
+        if(!areArgumentsValid(args)){
+            return printUsage(args[0]);
+        }
+        inputFile.open(args[1]);
+    }
+    
+    minifyFile(inputFile);
+    
+    return 0;
+}
 
-    ParserState parserState;
-    parserState.commentState = CommentType.None;
-    parserState.quoteState = QuoteType.None;
+bool areArgumentsValid(string[] args){
+    if(args.length != 2){
+        return false;
+    }
+    
+    return isValidFileName(args[1]);
+}
+
+bool isValidFileName(string fileName){
+    return exists(fileName) && isFile(fileName);
+}
+
+int printUsage(string programName){
+    stderr.writef("usage: %s <input_filename>\n", programName);
+    return 1;
+}
+
+void minifyFile(File file){
+    ParserState parserState = createParserState();
     
     char[] buf;
-    while(stdin.readln(buf)){
+    while(file.readln(buf)){
         parserState = minifyLine(buf, parserState);
         write(buf);
     }
